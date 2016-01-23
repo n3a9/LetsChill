8458e3d8d449908b7cdb4cc65ffae50c91e572bf
lifegood141 <aggarwal.neeraj141@gmail.com>
2016-01-23 16:22:02 -0700

    merged modals and filtering

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+         
