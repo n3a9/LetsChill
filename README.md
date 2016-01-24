dfacdc3571dd98d9ee598e0a62303cec7db7c23c
lifegood141 <aggarwal.neeraj141@gmail.com>
2016-01-24 03:44:53 -0700

    aesthetic updates

---
+++ README.md
@@ -0,0 +1,41 @@
+#LetsChill
+A web app to find a meetup location for two people in a local area. Given two locations, it will find options in between them that can be used to meet up such as cafes, mini golf, and other options.
+
+
+
+Developed @ HackArizona 2016
+
+##Usage
+Hosted at [http://aggarwalneeraj141.github.io/LetsChill/](http://aggarwalneeraj141.github.io/LetsChill/)
+
+##APIs Used
+Google Places API and Google Maps API
+
+##Contribute
+1. **Fork and clone repository**: Fork and clone the repository to your own profile.
+    Fork: Click the fork button on the top right hand corner.
+
+    ![fork](https://cloud.githubusercontent.com/assets/7104017/12533246/fe8d5a98-c1e6-11e5-93a6-81c4ffa81d54.png)
+    
+    In terminal clone: ```git clone [your cloned url]```
+    in the directory you wish to keep a local copy.
+2. **Update**: Make changes and updates that you wish to add.
+3. **Commit**: First, add the files you made changes to. ```git add [files]```
+    
+    Commit to your repository the changes you made: ```git commit -m "message"```
+4. **Push**: Push your commit to the branch you wish to update in terminal: ```git push origin [branch]```
+5. **Submit a pull request**: Go to your repository and click "Compare & pull request." Confirm you want to submit a pull request for your updates. 
+
+    ![pullrequest](https://cloud.githubusercontent.com/assets/7104017/12533289/70798ff4-c1e8-11e5-8c6b-c5916de487ee.png)
+
+##Contributers
+1. [Alex Jordache](http://alexjordache.me)
+2. [Jason Wong](http://jasonkcwong.com)
+3. [Syed Shah](http://www.linkedin.com/in/syedzafarshah6)
+4. [Neeraj Aggarwal](http://neerajaggarwal.com)
+
+##License
+Licensed under the ISC License.
+
+##Troubleshooting/Contact
+If having issues, contact aggarwal.neeraj141@gmail.com for help.
---
+++ public/js/map.js
@@ -0,0 +1,316 @@
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+var map;
+var infowindow;
+var input1;
+var input2;
+var autocomplete1;
+var autocomplete2;
+var lat1;
+var lon1;
+var lat2;
+var lon2;
+var markers = [];
+
+function submitLocations(){
+    if (input1.value==""||input2.value=="")
+        {
+            alert("One or more locations is empty!");
+            return;
+        }
+      autocomplete1.addListener('place_changed', function() {
+        infowindow.close();
+        var place = autocomplete1.getPlace();
+      
+        if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+        }
+        console.log(place);
+        
+        console.log(place.name);
+        
+        lat1=place.geometry.location.lat();
+        
+        lon1=place.geometry.location.lng();
+          
+
+
+    });
+    
+    autocomplete2.addListener('place_changed', function() {
+        infowindow.close();
+        var place = autocomplete2.getPlace();
+      
+        if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+        }
+        console.log(place);
+        
+        console.log(place.name);
+        
+        lat2=place.geometry.location.lat();
+        
+        lon2=place.geometry.location.lng();
+
+    });
+    
+    var midpoint = getMidpoint(lat1, lon1, lat2, lon2)
+    var midpointCoords = {lat: midpoint[0], lng: midpoint[1]};
+    
+    console.log(midpoint[0]);
+    
+    localStorage.setItem("mdpt", midpoint);
+    window.location.href = "results.html";
+}
+
+function initMap() {
+    
+    input1 = document.getElementById('autocomplete1');
+    autocomplete1 = new google.maps.places.Autocomplete(input1);
+    
+    input2 = document.getElementById('autocomplete2');
+    autocomplete2 = new google.maps.places.Autocomplete(input2);
+
+    infowindow = new google.maps.InfoWindow();
+    
+    autocomplete1.addListener('place_changed', function() {
+        infowindow.close();
+        var place = autocomplete1.getPlace();
+      
+        if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+        }
+        console.log(place);
+        
+        console.log(place.name);
+        
+        lat1=place.geometry.location.lat();
+        
+        lon1=place.geometry.location.lng();
+        
+                  console.log(lat1);
+          console.log(lon1);
+
+    });
+    
+    autocomplete2.addListener('place_changed', function() {
+        infowindow.close();
+        var place = autocomplete2.getPlace();
+      
+        if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+        }
+        console.log(place);
+        
+        console.log(place.name);
+        
+        lat2=place.geometry.location.lat();
+        
+        lon2=place.geometry.location.lng();
+        console.log(lat2);
+        console.log(lon2);
+
+    });
+    
+    var midpoint = getMidpoint(lat1, lon1, lat2, lon2);
+    var midpointCoords = {lat: midpoint[0], lng: midpoint[1]};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: midpointCoords,
+        zoom: 15
+    });
+
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: midpointCoords,
+        radius: 500
+    }, callback);
+    
+}
+
+function initMaptwo() {
+    var midpoint = localStorage.getItem("mdpt");
+    var n = midpoint.indexOf(",");
+    var midpointCoords = {lat: parseFloat(midpoint.substr(0,n)), lng: parseFloat(midpoint.substr(n+1,midpoint.length-1))};
+    
+    console.log(midpointCoords);
+    
+    infowindow = new google.maps.InfoWindow(); 
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: midpointCoords,
+        zoom: 15
+    });
+
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: midpointCoords,
+        radius: 500
+    }, callback);
+    
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        var html = "";
+        for (var i = 1; i < results.length; i++) {
+            createMarker(results[i]);
+            html += "<div class='locations-wrapper'><div id='left-results'><h1 class='name black-text'>" + results[i].name + "</h1>";
+            html += "</div>";
+//          console.log(results[i]);
+//            if (results[i].types.length > 0){
+//                var start = "<h3 class='type black-text'>";
+//                html += start;
+//            }
+//            var types = "";
+//            for (var k = 0; k < results[i].types.length; k++){
+//                if (k == results[i].types.length - 1){
+//                    var type = formatType(results[i].types[k]);
+//                    types += type;
+//                    html += types + "</h3></div>";
+//                } else {
+//                    var type = formatType(results[i].types[k]);
+//                    types += type + ", ";
+//                }
+//
+//            }
+            html += "<div id='right-results'><h4 class='address black-text'>" + results[i].vicinity + "</h4>";
+            
+            html += "</div></div>";
+            
+            
+            console.log(results[i].name); //get name
+            console.log(results[i].vicinity);
+            console.log(results[i].types);
+        }
+        document.getElementById('locations-wrapper').innerHTML = html;
+    }
+
+}
+
+function formatType(type){
+    if(!(type.indexOf("_") > -1)){
+        return type.charAt(0).toUpperCase() + type.slice(1);
+    } else {
+        var arr = [];
+//        var replaced = type.replace('_', ' ');
+        var words = type.split('_');
+        var string = "";
+        for (var i = 0; i < words.length; i++){
+            if (i != 0){
+                string += " ";
+            }
+            string += words[i].charAt(0).toUpperCase() + words[i].slice(1);
+        }
+        console.log(string);
+        return string;
+        
+    }
+
+}
+
+
+function formatType(type){
+    if(!(type.indexOf("_") > -1)){
+        return type.charAt(0).toUpperCase() + type.slice(1);
+    } else {
+        var arr = [];
+//        var replaced = type.replace('_', ' ');
+        var words = type.split('_');
+        var string = "";
+        for (var i = 0; i < words.length; i++){
+            if (i != 0){
+                string += " ";
+            }
+            string += words[i].charAt(0).toUpperCase() + words[i].slice(1);
+        }
+        console.log(string);
+        return string;
+        
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+    markers.push(marker);
+}
+
+
+
+
+function clearMarkers() {
+    setMapOnAll(null);
+    var html = "";
+    document.getElementById('locations-wrapper').innerHTML = html;
+}
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+        
+function getMidpoint(lat1, lng1, lat2, lng2) {
+    Math.degrees = function(rad) {
+        return rad * (180 / Math.PI);
+    }
+    Math.radians = function(deg) {
+        return deg * (Math.PI / 180);
+    }
+    lat1 = Math.radians(lat1);
+    lng1 = Math.radians(lng1);
+    lat2 = Math.radians(lat2);
+    lng = Math.radians(lng2);
+    bx = Math.cos(lat2) * Math.cos(lng - lng1);
+    by = Math.cos(lat2) * Math.sin(lng - lng1);
+    lat3 = Math.atan2(Math.sin(lat1) + Math.sin(lat2), Math.sqrt((Math.cos(lat1) + bx) * (Math.cos(lat1) + bx) + Math.pow(by, 2)));
+    lon3 = lng1 + Math.atan2(by, Math.cos(lat1) + bx);
+    return [Math.degrees(lat3), Math.degrees(lon3)]
+}
+
+function checkedUpdate() {
+    // Retrieve the checked radiobuttons
+    var checkedArr = [];
+    
+    var checkItems = $('.checkitem');
+    
+    for (var i = 0; i < checkItems.size(); i++) {
+        if (checkItems[i].checked == true){
+            checkedArr.push(checkItems[i].name);
+        }
+    }
+    console.log(checkedArr);
+    var midpoint = localStorage.getItem("mdpt")
+    var n = midpoint.indexOf(",");
+    var midpointCoords = {lat: parseFloat(midpoint.substr(0,n)), lng: parseFloat(midpoint.substr(n+1,midpoint.length-1))};
+    
+    console.log(midpointCoords);
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: midpointCoords,
+        zoom: 15
+    });
+    clearMarkers();
+    
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: midpointCoords,
+        radius: 500,
+        types: checkedArr
+    }, callback);
+    
+}
